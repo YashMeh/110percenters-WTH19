3e4e44aa30ecbd1b4f38210aa5a3c220dbb72043
YashMeh <mehrotra.yash9@gmail.com>
2019-03-11 00:50:08 +0530

    Added shake feature

--- routes/pageRoutes.js
+++ routes/pageRoutes.js
@@ -2,7 +2,7 @@ const express=require("express")
 const router=express.Router();
 
 router.get("/",(req,res)=>{
-    res.send("Working ");
+    res.sendFile("home.html",{root:__dirname+"/../views"})
 })
 
 module.exports=router;
