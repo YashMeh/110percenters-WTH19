7a2c4c0bef440dd5df3549d1c697815738d6ecb8
YashMeh <mehrotra.yash9@gmail.com>
2019-03-11 01:41:31 +0530

    Added default page

--- routes/verifyTokens.js
+++ routes/verifyTokens.js
@@ -2,7 +2,7 @@ var jwt = require('jsonwebtoken');
 function verifyToken(req, res, next) {
   var token = req.headers['x-access-token'];
   if (!token)
-    return res.status(403).sendFile("register.html",{root:__dirname+"/../views"});
+    return res.sendFile("login.html",{root:__dirname+"/../views"});
   jwt.verify(token, process.env.secretKey, function(err, decoded) {
     if (err)
     return res.status(500).send({ auth: false, message: 'Failed to authenticate token.' });
